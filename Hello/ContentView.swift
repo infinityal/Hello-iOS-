3725e508a56f665a538e2a3217ba71c0b7507d2a
infinityal <zhjzh15@uw.edu>
2022-04-05 22:13:06 -0700

    finished

---
+++ Hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by 郑智健 on 4/5/22.
+//  Copyright © 2022 INFO 449. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Go Seahawks!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
